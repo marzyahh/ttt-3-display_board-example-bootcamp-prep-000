8833729c670220528239d248360fce1a8b69d4e2
marzyah <marzyahhooseini74@gmail.com>
2020-05-22 09:36:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   
-  
+  puts "  |  |  "
+  puts "------------"
+  puts "  |  |  "
+irb(main):012:1> puts "------------"
+irb(main):013:1> puts "  |  |  "
 end
